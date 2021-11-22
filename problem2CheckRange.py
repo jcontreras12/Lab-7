dfa0f3c282c151ddf46619eab662487e5e331997
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-11-22 08:49:07 -0600

    Update problem2CheckRange.py

--- problem2CheckRange.py
+++ problem2CheckRange.py
@@ -2,7 +2,13 @@
 # Date 11/13/21
 # problem 2 write a python function to check weather a number is in a given range. range is 1-10
 def Range(i):
-    if i in range(1-11):
+    #if i in range(1-11): incorrect use of parameter.
+     if i in range(1, 11):
         print(i, " number is in range")
     else:
         print(i, "number is not in range")
+        
+#Function Call Statement
+num = int(input("enter a number to check if it is in range (1, 10):"))
+Range(num)
+        
