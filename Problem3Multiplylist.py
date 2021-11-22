dc704eca27c8000f3be06456a7caf8e379c5cea7
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-11-22 08:40:32 -0600

    Update Problem3Multiplylist.py

--- Problem3Multiplylist.py
+++ Problem3Multiplylist.py
@@ -1,9 +1,12 @@
 # Jose Contreras
 # Date 11/13/21
 # problem 3, write a Python function to multiply all the numbers in a list
-n = (5, 2, 7, -1)
+n = (5, 2, 7, -1) # <= n is a set.  list=> n = [5, 2, 7, -1]
 def multi(n):
     x = 1
     for i in n:
         x = x * i
-        print(x)
+        #print(x) it should be outside of the for loop
+    print(x)
+multi(n)   #need to call multi function with passing a set n 
+ 
